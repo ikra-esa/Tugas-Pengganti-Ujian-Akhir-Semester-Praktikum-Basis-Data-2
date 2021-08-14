998ae18b397d5b5b0c506ccef6dbd08fc7dae302
ikra-esa <84899851+ikra-esa@users.noreply.github.com>
2021-08-14 14:23:34 +0700

    Delete application directory

--- application/views/admin/_partials/jq.php
+++
@@ -1,3 +0,0 @@
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.24/datatables.min.js"></script>
